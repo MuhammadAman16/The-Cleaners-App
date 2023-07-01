6a685d1e13dd07fc3e8515f21901bbf80f6882f4
MuhammadAman16 <mohammedaman5253@gmail.com>
2023-07-01 18:57:05 +0500

    modified

--- App.js
+++ App.js
@@ -3,9 +3,17 @@ import { StyleSheet, Text, View } from "react-native";
 import LoginScreen from "./app/screens/LoginScreen";
 import RegisterScreen from "./app/screens/RegisterScreen";
 import HomeScreen from "./app/screens/HomeScreen";
+import AppNavigator from "./app/navigation/AppNavigator";
+import AuthNavigator from "./app/navigation/AuthNavigator";
+import { NavigationContainer } from "@react-navigation/native";
+import ListingEditScreen from "./app/screens/DC_CheckOut";
 
 export default function App() {
-  return <HomeScreen />;
+  return (
+    <NavigationContainer>
+      <ListingEditScreen />
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({});
